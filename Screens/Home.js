85504de288f68feb1898b5fca19cc6ed22cca5de
Spaceman213 <73033227+Spaceman213@users.noreply.github.com>
2022-08-15 19:15:17 -0500

    ui for homepage

--- Screens/Home.js
+++ Screens/Home.js
@@ -2,7 +2,8 @@ import { TouchableOpacity, Text, Image, View } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { useContext } from "react";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
-
+import IonIcons from "react-native-vector-icons/Ionicons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import colors from "../styles/Colors";
 import LoginContext from "../utils/LoginContext";
 
@@ -29,17 +30,17 @@ function PointCard(props) {
 
   return (
     <View style={{ height: 50, width: '90%', flexDirection: 'row', paddingRight: 8 }}>
-      <View style={{ width: '20%', justifyContent: 'center' }}>
+      <View style={{ marginRight: 10, justifyContent: 'center' }}>
         <Image
           source={props.image}
           resizeMode="contain"
-          style={{ width: 50, height: 50 }}
+          style={{ width: 40, height: 40 }}
         />
       </View>
       <View style={{ width: '80%', paddingLeft: 5, justifyContent: 'center' }}>
         <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 6 }}>
-          <Text style={{ fontFamily: 'Poppins_600SemiBold', fontSize: 20, color: '#262626' }}>{props.name}</Text>
-          <Text style={{ fontFamily: 'Poppins_600SemiBold', fontSize: 20, color: '#262626' }}>{props.points + '/' + props.totalPoints}</Text>
+          <Text style={{ fontFamily: 'Poppins_600SemiBold', fontSize: 16, color: '#262626' }}>{props.name}</Text>
+          <Text style={{ fontFamily: 'Poppins_600SemiBold', fontSize: 16, color: '#262626' }}>{props.points + '/' + props.totalPoints}</Text>
         </View>
         <View>
           <View style={{ width: '100%', borderRadius: 3, marginTop: 4, height: 4, backgroundColor: '#F2F2F2' }}></View>
@@ -49,27 +50,33 @@ function PointCard(props) {
     </View>
   );
 }
+function SubRequirement(props) {
+  const color = props.complete ? '#D5D5D5' : '#5F6368';
+  return (
+    <View style={{ width: '33%', padding: 6, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+      {props.icon}
+      <View style={{ width: '100%', paddingLeft: 2, paddingRight: 2 }}>
+        <Text style={{ textAlign: 'center', color: color, fontFamily: 'Poppins_600SemiBold', fontSize: 12 }}>{props.label}</Text>
+      </View>
+      <View style={{ borderColor: color, width: 16, height: 16, borderWidth: 1, borderRadius: 4, justifyContent: 'center', alignItems: 'center' }}>
+        {
+          props.complete ?
+            <FontAwesome5Icon
+              name={'check'}
+              size={10}
+              color={'#85C67E'}
+            /> : null
+        }
+      </View>
+    </View>
+  )
+}
 
 function PointDisplay(props) {
-  const navigation = useNavigation();
-  
+
   return (
-    <View style={{ backgroundColor: '#FFFFFF', width: '90%', height: 360, borderWidth: 1, borderColor: '#D8D8D8', borderRadius: 10 }}>
-      <View style={{ height: 60, width: '100%', borderBottomWidth: 1, borderColor: '#DBDBDB', alignItems: 'center', paddingLeft: 20, flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20 }}>
-        <Text style={{ fontSize: 22, fontFamily: 'Poppins_600SemiBold', color: '#262626' }}>Points</Text>
-        <TouchableOpacity
-          style={{ width: 46, height: 46, alignItems: 'center', justifyContent: 'center', backgroundColor: colors.universityColor + '50', borderRadius: 23 }}
-          onPress={() => {
-            navigation.navigate('Waitlist');
-          }}
-        >
-          <FontAwesome5Icon
-            name='clipboard-list'
-            color={colors.universityColor}
-            size={28}
-          />
-        </TouchableOpacity>
-      </View>
+
+    <View style={{ backgroundColor: '#FFFFFF', width: '90%', paddingBottom: 10, borderWidth: 1, borderColor: '#D8D8D8', borderRadius: 10, alignItems: 'center' }}>
       <View style={{ alignItems: 'center', justifyContent: 'space-evenly', height: 300 }}>
         <PointCard
           name={'Philanthropy'}
@@ -93,10 +100,66 @@ function PointDisplay(props) {
           color={'#EFA039'}
         />
       </View>
+      <View style={{ width: '90%', flexWrap: 'wrap', flexDirection: 'row' }}>
+        <SubRequirement
+          label={'Dues'}
+          complete={true}
+          icon={
+            <FontAwesome5Icon
+              name={'money-bill-wave'}
+              size={20}
+              color={true ? '#D5D5D5' : '#5F6368'}
+            />
+          }
+        />
+        <SubRequirement
+          label={'Interviews'}
+          complete={true}
+          icon={
+            <IonIcons
+              name={'chatbubble-ellipses'}
+              size={20}
+              color={true ? '#D5D5D5' : '#5F6368'}
+            />
+          }
+        />
+        <SubRequirement
+          label={'Attendance'}
+          complete={false}
+          icon={
+            <MaterialCommunityIcons
+              name={'clock-check'}
+              size={20}
+              color={false ? '#D5D5D5' : '#5F6368'}
+            />
+          }
+        />
+
+      </View>
+
     </View>
   );
 }
-
+function HomePageHeader() {
+  const navigation = useNavigation();
+  return (
+    <View style={{ marginBottom: 10, width: '90%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      <Text style={{ fontSize: 22, fontFamily: 'Poppins_600SemiBold', color: '#262626' }}>Hello, Name</Text>
+      <TouchableOpacity
+        style={{ width: 46, height: 46, alignItems: 'center', justifyContent: 'center', backgroundColor: colors.universityColor + '50', borderRadius: 23 }}
+        onPress={() => {
+          navigation.navigate('Waitlist');
+        }}
+      >
+        <FontAwesome5Icon
+          name='clipboard-list'
+          color={colors.universityColor}
+          size={28}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
 export function HomePage() {
   const loginContext = useContext(LoginContext);
 
@@ -119,11 +182,8 @@ export function HomePage() {
   }
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#FAFAFA', alignItems: "center", justifyContent: "space-between" }}>
-      <View style={{
-        justifyContent: "center"
-      }}
-      />
+    <View style={{ flex: 1, backgroundColor: '#FAFAFA', alignItems: "center", paddingTop: 30 }}>
+      <HomePageHeader />
       <PointDisplay
         philanthropyPoints={loginContext.currentUser.philanthropyPoints}
         socialPoints={loginContext.currentUser.socialPoints}
@@ -136,7 +196,7 @@ export function HomePage() {
         activeInterviews={loginContext.currentUser.activeInterviews}
         isPledge={loginContext.currentUser.status === "pledge"}
       />
-      <View style={{ width: '90%', flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+      <View style={{ position: 'absolute', width: '90%', flexDirection: 'row', alignItems: 'center', bottom: 10 }}>
         <SubmitPoints address="Submit" title="Submit" />
       </View>
     </View>
