1423f8932b980dd05fd7a664dd2feeaef2ce1688
aknbln <73264040+aknbln@users.noreply.github.com>
2022-10-11 16:00:55 -0500

    splash screen and logo changed

--- Screens/SplashScreen.js
+++ Screens/SplashScreen.js
@@ -1,15 +1,20 @@
-import { SafeAreaView, View, Image, ActivityIndicator, Text } from "react-native";
-import { useCallback } from "react";
+import { SafeAreaView, View, Image, ActivityIndicator, Text, Animated } from "react-native";
+import { useCallback, useEffect } from "react";
 import LoginContext from "../utils/LoginContext";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { useNavigation } from "@react-navigation/native";
-
 
 export function SplashScreenPage() {
   const navigation = useNavigation();
   const loginContext = useContext(LoginContext);
   const appIsReady = loginContext.appIsReady;
 
+  const fading = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    
+
+  }, []);
 
   if (appIsReady) {
     navigation.navigate("Navigation");
@@ -19,11 +24,9 @@ export function SplashScreenPage() {
       style={{ flex: 1, justifyContent: 'center', backgroundColor: 'white' }}>
       <Image
         style={{ width: 300, height: 300, flexDirection: 'row', alignSelf: 'center' }}
-        resizeMode='cover'
-        source={require("../assets/icon.png")}></Image>
+        // resizeMode='cover'
+        source={require("../assets/splash.png")}></Image>
 
-      {/* <Text style = {{fontFamily: 'Poppins_600SemiBold', alignSelf: 'center', color: 'black', fontSize: 50, paddingRight: '5%'}} 
-        >NEXUS</Text> */}
       <View
         style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '10%' }}>
 
